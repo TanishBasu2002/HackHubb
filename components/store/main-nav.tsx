da77a96d2155c7bb2b1d3d70cbbd077f7c1a2eb2
Tanish Basu <tanishbasu50@gmail.com>
2023-09-07 13:37:10 +0530

    Addeed settings for chatroom and invite forchatroom along with updating the general code nd dependencies

--- components/store/main-nav.tsx
+++ components/store/main-nav.tsx
@@ -1,28 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
-
+/**
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+import { usePathname } from 'next/navigation' */
 
-interface MainNavProps{
+
+/**interface MainNavProps{
     data:any
-}
-const MainNav = ({data}:MainNavProps) => {
-    const pathname = usePathname();
+} */
+const MainNav = () => {
+   /** const pathname = usePathname();
     const routes = data.map((route)=>({
-        href:`/hackstore/ctegory/${route.id}`,
+        href:`/hackstore/category/${route.id}`,
         label:route.name,
-        active:pathname === `/hackstore/xcategory/${route.id}`
-    }))
+        active:pathname === `/hackstore/category/${route.id}`
+    })) */
   return (
     <nav className='mx-6 flex items-center space-x-4 lg:space-x-6'>
-      {routes.map((route)=>(
+      Navbar{/**{routes.map((route)=>(
         <Link key={route.href} href={route.href} className={cn("text-sm font-medium transition-colors hover:text-white",route.active ? "text-white":"text-neutral-400")}>
             {route.label}
         </Link>
-      ))}
+      ))} */}
     </nav>
   )
 }
