917fc98e960817780e3c4cc89db30b8daa6b03c9
TanishBasu2002 <tanishbasu50@gmail.com>
2023-10-19 23:01:29 +0530

    fixed some errors

--- app/(storage)/(cloud)/storage/(main)/_components/FileStorageHeader.tsx
+++ app/(storage)/(cloud)/storage/(main)/_components/FileStorageHeader.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 "use client"
-import { Button } from "../ui/button"
+import { Button } from "../../../../../../components/ui/button"
 
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog"
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../../../../../../components/ui/alert-dialog"
 import { useRouter } from "next/navigation";
+import Dropzone from "./dropzone";
+import { Label } from "@/components/ui/label";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+
 
 const FileStorageHeader = () => {
   const router = useRouter();
@@ -55,9 +59,27 @@ const FileStorageHeader = () => {
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
-        <Button onClick={onClick} variant="outline" className="bg-gradient-to-tr from-gray-500 via-purple-500 to-rose-600 hover:bg-gradient-to-tr hover:from-rose-600 hover:via-sky-600 hover:to-emerald-600 hover:text-light-1 border-none"> 
-            File Converter
-        </Button>
+      <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="bg-gradient-to-tr from-gray-500 via-purple-500 to-rose-600 hover:bg-gradient-to-tr hover:from-rose-600 hover:via-sky-600 hover:to-emerald-600 hover:text-light-1 border-none">File Convertor</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Convert Files</DialogTitle>
+          <DialogDescription>
+           Convert Files and Videos
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Drop Files Here
+            </Label>
+            <Dropzone />
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
     </div>
   )
 }
