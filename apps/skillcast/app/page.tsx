80c2c2a4e0e2da389b92585f64009b9f3d677fa0
TanishBasu2002 <tanishbasu50@gmail.com>
2024-01-04 14:41:32 +0530

    final

---
+++ apps/skillcast/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-red-500">Skillcast</p>
+      <Button variant="default" size={"lg"}>
+        Cast
+      </Button>
+    </>
+  );
+}
