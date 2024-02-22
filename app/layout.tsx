bdfd54b8d45cb10ea496451dc393502bfee3b042
Tanish Basu <tanishbasu50@gmail.com>
2024-02-22 17:23:50 +0530

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import { ToasterProvider } from "@/lib/providers/toaster";
 export const metadata = {
   title: "HackSpace",
@@ -19,7 +19,6 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <SpeedInsights />
       <html lang="en">
         <body suppressHydrationWarning>
           <ToasterProvider />
