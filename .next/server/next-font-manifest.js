1295423a454915e4cbc26548c1c764ce6fa3674e
Tanish Basu <122549983+TanishBasu2002@users.noreply.github.com>
2024-02-22 17:10:49 +0530

    finish

--- .next/server/next-font-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__NEXT_FONT_MANIFEST =
-  '{"pages":{},"app":{"/media/tanish/New Volume/HackHubb/HackHubb/app/(root)/layout":["static/media/01af0fc7b4278e65-s.p.woff2"],"/media/tanish/New Volume/HackHubb/HackHubb/app/(chat)/layout":["static/media/01af0fc7b4278e65-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
